322c8a1bed498cd72856dc23b20176e378af2e4a
Pankajsiwach123321 <pankusiwach000@gmai.com>
2024-04-26 11:05:36 +0530

    update ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 export const Contact = () => {
-  const [pop, setPop] = useState(false);
   const [fromdata, setFromdata] = useState({
     name: "",
     username: "",
@@ -24,7 +23,7 @@ export const Contact = () => {
       name: /^[a-zA-Z\s]+$/,
       username:
         /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-      message: /^[A-Za-z0-9.]{5,1000}$/,
+      message: /^[A-Za-z0-9.\s]{5,1000}$/,
     };
     const error = {};
     if (!regex.name.test(fromdata.name)) {
@@ -38,18 +37,6 @@ export const Contact = () => {
     }
     setFromerror(error);
     if (Object.keys(error).length === 0) {
-      setFromdata({
-        name: "",
-        number: "",
-        password: "",
-        confirmpassword: "",
-      });
-      setFromerror({
-        name: "",
-        number: "",
-        password: "",
-        confirmpassword: "",
-      });
       emailjs
         .sendForm("service_p07qtes", "template_91opoyo", form.current, {
           publicKey: "qN_3TJV48NGiMwDpX",
@@ -57,6 +44,17 @@ export const Contact = () => {
         .then(
           () => {
             console.log("SUCCESS!");
+            setFromdata({
+              name: "",
+              username: "",
+              message: "",
+            });
+            setFromerror({
+              name: "",
+              number: "",
+              password: "",
+              confirmpassword: "",
+            });
           },
           (error) => {
             console.log("FAILED...", error.text);
@@ -66,55 +64,87 @@ export const Contact = () => {
   };
 
   return (
-    <section className=" min-h-screen  flex flex-col justify-center items-center px-3">
-      <h2 className=" text-black uppercase font-mono text-5xl font-bold">
+    <section className=" min-h-screen bg-black  flex flex-col justify-center items-center px-5">
+      <h2 className=" bg-text bg-clip-text text-transparent Helvetica  uppercase Helvetica text-5xl font-bold">
         Email Js
       </h2>
       <form
         ref={form}
         onSubmit={sendEmail}
-        className=" bg-white border-orange-600  border-2 rounded-xl mt-5 max-w-[520px] mx-auto w-full px-10 py-20 shadow-[0_0_4px_1px_#ff0000]"
+        className=" bg-img bg-cover border-[#31302C] border-4 bg-no-repeat   rounded-xl mt-5 max-w-[520px] mx-auto w-full px-10 py-20 "
       >
-        <label className=" text-orange-700 font-mono text-3xl block">
+        <label className=" bg-text bg-clip-text text-transparent Helvetica text-2xl font-bold block">
           Name
         </label>
         <input
           onChange={inputCon}
           type="text"
           name="name"
-          className={`${
-            Fromerror.name && "!text-red-700"
-          } border-2 outline-0 placeholder:font-mono  placeholder:text-green-400 text-black mt-3 rounded-lg border-green-500 px-3 py-5 w-full`}
+          placeholder="Name"
+          value={fromdata.name}
+          className={` border outline-0 placeholder:Helvetica  placeholder:text-[#B3B0AD] bg-transparent text-[#B3B0AD] mt-2 rounded-lg border-[#31302C] px-5 py-[17px] w-full`}
         />
         {Fromerror.name && <p>{Fromerror.name}</p>}
-        <label className=" text-orange-700 font-mono text-3xl block">
+        <label className=" bg-text bg-clip-text text-transparent Helvetica text-2xl font-bold mt-5 block">
           Email
         </label>
         <input
           onChange={inputCon}
           type="email"
+          value={fromdata.username}
           name="username"
-          className={`${
-            Fromerror.username && "!text-red-700"
-          }  border-2 outline-0 placeholder:font-mono  placeholder:text-green-400 text-black mt-3 rounded-lg border-green-500 px-3 py-5 w-full`}
+          placeholder="Email"
+          className={` border-2 outline-0 placeholder:Helvetica bg-transparent  placeholder:text-[#B3B0AD] text-[#B3B0AD] mt-2 rounded-lg border-[#31302C] px-5 py-[17px] w-full`}
         />
         {Fromerror.username && <p>{Fromerror.username}</p>}
-        <label className=" text-orange-700 font-mono text-3xl block">
+        <label className=" bg-text bg-clip-text text-transparent Helvetica text-2xl font-bold mt-5 block">
           Message
         </label>
         <textarea
           onChange={inputCon}
           name="message"
-          className={`${
-            Fromerror.message && "!text-red-700"
-          } border-2 outline-0 placeholder:font-mono  placeholder:text-green-400 text-black mt-3 rounded-lg border-green-500 px-3 py-5 w-full`}
+          value={fromdata.message}
+          placeholder="Message"
+          className={`border-2 resize-none outline-0 placeholder:Helvetica bg-transparent  placeholder:text-[#B3B0AD] text-[#B3B0AD] mt-2 rounded-lg border-[#31302C] px-5 py-[17px] w-full`}
         />
         {Fromerror.message && <p>{Fromerror.message}</p>}
-        <input
-          type="submit"
-          value="Send"
-          className={`px-5 py-2 mt-6 bg-blue-600 font-mono rounded-xl text-white hover:text-blue-600 duration-300 hover:bg-white hover:border-blue-600 border border-transparent cursor-pointer`}
-        />
+        <div className=" group inline-flex items-center gap-2 mt-6 border-b border-b-[#F4EAD8] ">
+          <input
+            type="submit"
+            value="Send"
+            className={` py-2   bg-text bg-clip-text text-transparent Helvetica rounded-xl   duration-300   border border-transparent cursor-pointer`}
+          />
+          <span className=" group-hover:translate-x-1 duration-300">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 12L13 18M19 12L13 6M19 12L5 12"
+                stroke="url(#paint0_linear_51109_714)"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_51109_714"
+                  x1="19"
+                  y1="6"
+                  x2="4.69489"
+                  y2="16.925"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#F4EAD8" />
+                  <stop offset="1" stop-color="#E0B764" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+        </div>
       </form>
     </section>
   );
